919c4e821ce4e80e20af4c3d715eded53cda92a5
Zeeshan Qureshi <pmz5164300@gmail.com>
2026-07-27 10:44:14 +0500

    added openZeppelin librarey

--- SmartContracts/contracts/Erc20Factory.sol
+++ SmartContracts/contracts/Erc20Factory.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-error inSufficientBalance(uint256 required, uint256 available);
-error inSufficientAllowance(uint256 required, uint256 available);
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 error InsufficientPayment();
 error InvalidNameOrSymbol();
 error ZeroAmount();
@@ -10,70 +10,12 @@ error unAuthorized();
 error WithdrawFailed();
 
 
-contract CustomERC20{
-    string public  name;
-    string public  symbol;
-    uint256 public totalSupply; 
-
-    mapping(address => uint256) public balances;
-    mapping(address => mapping (address=> uint256)) public allowances;
-
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Approval(address indexed from, address to, uint amount);
-
-
-    constructor(string memory _name, string memory _symbol, uint256  _initialSupply, address creator) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balances[creator] = totalSupply;
-
-        emit Transfer(address(0), creator, totalSupply);
+contract ERC20Token is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply, address creator)
+        ERC20(_name, _symbol)
+    {
+        _mint(creator, _initialSupply);
     }
-
-    function transfer(address _to, uint256 _amount) public {
-        if(balances[msg.sender] < _amount) {
-        revert inSufficientBalance(_amount, balances[msg.sender]);}
-        if ( _amount == 0) {
-           revert ZeroAmount();}
-
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
-
-        emit Transfer(msg.sender, _to, _amount );
-    }
-
-    function approve(address _spender, uint256 _amount) public {
-        if ( _amount == 0) {
-           revert ZeroAmount();}
-
-        allowances[msg.sender][_spender] = _amount;
-
-        emit Approval(msg.sender, _spender, _amount);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _amount) public {
-        if ( _amount == 0) {
-           revert ZeroAmount();}   
-        if (balances[_from] < _amount) {
-            revert inSufficientBalance(_amount, balances[_from]);}
-        if( allowances[_from][msg.sender] < _amount){
-            revert inSufficientAllowance(_amount, allowances[_from][msg.sender]);}
-
-
-        balances[_from] -= _amount; 
-        allowances[_from][msg.sender] -=  _amount;
-        balances[_to] += _amount;
-
-        emit Transfer(_from, _to, _amount);
-
-    }
-
-    function balanceOf(address _address) external view returns(uint256) {
-        return balances[_address];
-    }
-
-
 }
 
 contract Erc20Factory{
@@ -98,7 +40,7 @@ contract Erc20Factory{
         if (_initialSupply == 0) {
             revert ZeroAmount();}
 
-        CustomERC20  erc20 = new CustomERC20(_name, _symbol, _initialSupply, msg.sender);
+        ERC20Token  erc20 = new ERC20Token(_name, _symbol, _initialSupply, msg.sender);
        address tokenAddress = address(erc20);
 
         tokensByUser[msg.sender].push(tokenAddress);
@@ -125,9 +67,9 @@ contract Erc20Factory{
 
             uint256 oldFee = fee;
 
-         fee = _newFee   
+         fee = _newFee;
 
-         emit FeeUpdated(oldFee, fee)  
+         emit FeeUpdated(oldFee, fee);  
       }      
 
       function totalTokensDeployed() external view returns(uint256) {
